aa72f9aaaebcd7f204ecac5758d5c487e1366613
Willem <willemarf@gmail.com>
2018-04-19 09:49:13 -0300

    echo api roles for url

--- api.py
+++ api.py
@@ -1,7 +1,42 @@
 from flask import Flask, request, render_template, jsonify
+from functools import wraps
+
+
+TOKEN = '123'
 
 
 app = Flask(__name__)
+
+
+def requires_roles(*roles):
+    def wrapper(f):
+        @wraps(f)
+        def wrapped(*args, **kwargs):
+            if get_current_user_role() not in roles:
+                return error_response()
+            return f(*args, **kwargs)
+        return wrapped
+    return wrapper
+
+def get_current_user_role():
+    # pega permissao do usuario
+    return 'user'
+
+# def auth_required(f):
+#     @wraps(f)
+#     def decorated(*args, **kwargs):
+#         try:
+#             token = request.headers['Authorization']
+#             if token is None:
+#                 return {'state': 'fail', 'message': 'Authorization required'}, 403
+#             if token != TOKEN:
+#                 return {'state': 'fail', 'message': 'Invalid token'}, 403
+#             return f(token=token, *args, **kwargs)
+#         except Exception as e:
+#             return {'state': 'fail', 'message': str(e)}, 500
+
+#     return decorated 
+
 
 @app.route('/')
 def hello_world():
@@ -11,6 +46,12 @@ def hello_world():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template('hello.html', name=name)
+
+@app.route('/user/')
+@requires_roles('admin', 'user')
+def user_page():
+    return "You've got permission to access this page."
+
 
 @app.route('/user/<username>')
 def show_user_profile(username):
@@ -40,6 +81,7 @@ def about():
     return 'about'
 
 @app.route('/getuser')
+# @auth_required
 def get_current_user():
     return jsonify(
         username="Willem",
@@ -52,4 +94,4 @@ if __name__ == '__main__':
 
 # curl -H 'Content-Type: application/json' -X POST -d '{"username": "will"}' http://localhost:5000/user/willem
 # curl -X POST -d '{"username": "will"}' http://localhost:5000/user/willem
-# curl -X POST http://localhost:5000/user/willem
+# curl -X POST -h {'Authorization': '123'} http://localhost:5000/getuser
